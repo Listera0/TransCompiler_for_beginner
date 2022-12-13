670e19dd0ab38b0807006f64b89831611449dda1
Listera <96122338+Listera0@users.noreply.github.com>
2022-12-14 01:07:21 +0900

    2022_12_14

--- [3] 구현/TransCompiler for beginner/app.py
+++ [3] 구현/TransCompiler for beginner/app.py
@@ -9,11 +9,19 @@ app = Flask(__name__)
 @app.route('/', methods = ['POST', 'GET'])
 def Start():
     if request.method == 'POST':
-        code = str(request.form["input_field"])
-        input_language = str(request.form["input_language"])
-        output_language = str(request.form["output_language"])
-        result = str(Converting(code, input_language, output_language))
-        return render_template("Main.html",origin = code , value = result)
+        submit = str(request.form["submit_type"])
+        if submit == "번역":
+            code = str(request.form["input_field"])
+            input_language = str(request.form["input_language"])
+            output_language = str(request.form["output_language"])
+            result = str(Converting(code, input_language, output_language))
+            return render_template("Main.html",origin = code , value = result)
+        elif submit == "⇋":
+            temp1 = str(request.form["input_field"])
+            temp2 = str(request.form["output_field"])
+            return render_template("Main.html", origin = temp2, value = temp1)
+        else:
+            return render_template("Main.html", value = '')
     else:
         return render_template("Main.html", value = '')
 
@@ -24,26 +32,21 @@ def Converting( code, in_lan, out_lan ):
     
     if in_lan == "1":
         temp1 = C.Extraction(code)
-        pass
     elif in_lan == "2":
         temp1 = CS.Extraction(code)
     elif in_lan == "3":
         temp1 = CPP.Extraction(code)
-        pass
     elif in_lan == "4":
         temp1 = Python.Extraction(code)
-        pass
     elif in_lan == "5":
         temp1 = Java.Extraction(code)
     
     if out_lan == "1":
         temp2 = C.ConvertCodeToC(temp1, 0)
-        pass
     elif out_lan == "2":
         temp2 = CS.ConvertCodeToCS(temp1, 0)
     elif out_lan == "3":
         temp2 = CPP.ConvertCodeToCPP(temp1, 0)
-        pass
     elif out_lan == "4":
         temp2 = Python.ConvertCodeToPython(temp1, 0)
     elif out_lan == "5":
